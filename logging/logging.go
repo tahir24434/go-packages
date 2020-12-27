e5640efec00cd1a0a68bb3024468b015908d3d27
Tahir Rauf <rautahir@amazon.com>
2020-12-27 00:00:33 -0800

    Add line and file number to logging package

--- logging/logging.go
+++ logging/logging.go
@@ -3,6 +3,7 @@ package logging
 import (
     "fmt"
     "time"
+    "runtime"
 )
 
 var debug bool
@@ -15,6 +16,17 @@ func Log(statement string) {
     if !debug {
         return
     }
-
-    fmt.Printf("%s %s\n", time.Now().Format(time.RFC3339), statement)
+    
+    var file string
+    var line int
+    var ok bool
+    // Caller reports file and line number information about function invocations on the calling goroutine's stack. 
+    // The argument skip is the number of stack frames to ascend, with 0 identifying the caller of Caller. 
+    _, file, line, _ = runtime.Caller(1)
+    //if !ok {
+    //    file = "???"
+    //    line = 0
+    //}
+    fmt.Printf("%s:%d: %s %s\n", file, line, time.Now().Format(time.RFC3339), statement)
+    
 }
